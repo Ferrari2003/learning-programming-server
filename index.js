b82970f7d0a403662a90829dd9744b371832c6e2
Al-Amin Hossain <al-aminhossain@Al-Amins-MacBook-Air.local>
2022-10-27 00:11:30 +0600

    Course Server Ready

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+const courses = require("./data/data.json");
+console.log(courses);
+
+app.get("/", (req, res) => {
+  res.send(courses);
+});
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  console.log(id);
+  const singleCourse = courses.find((c) => c.id === id);
+  res.send(singleCourse);
+});
+app.listen(port, () => {
+  console.log("server running port:", port);
+});
